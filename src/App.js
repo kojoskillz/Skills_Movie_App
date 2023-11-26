0b57433cf791a554f7c32510bb8200a4513e6c5c
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2023-11-26 02:00:28 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+// API_KEY = 3104513f
+
+
+const API_URL ='http://www.omdbapi.com?apikey=3104513f';
+
+const movie = {
+    
+          "Title": "Batman v Superman: Dawn of Justice",
+          "Year": "2016",
+          "imdbID": "tt2975590",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+        
+       
+}
+
+
+const App = () => {
+
+    const [ searchTerm, setSearchTerm] = useState([]) ;
+    const [movies, setMovies] = useState('');
+    const searchMovies = async (title) => {
+    const response = await fetch (`${API_URL}&s=${title}`);        
+    const data = await response.json();    
+ 
+
+    setMovies(data.Search);
+}
+
+    useEffect( () => {
+        searchMovies('Superman')
+}, []);
+
+return (
+
+    <div className='app'>
+            <h1>
+                SkillzMovieHub
+            </h1>      
+
+            <div className='search'>
+               <input type="text" placeholder='Search for movies' value={searchTerm} 
+               onChange={(e) => setSearchTerm(e.target.value)} />
+               <img src={SearchIcon} alt="searchicon" onClick={() => searchMovies(searchTerm)} />
+            </div>
+    
+  {
+    movies?.length > 0 
+    ? (
+        
+    <div className='container'>
+       {movies.map((movie) => (< MovieCard movie={movie}/>))}
+   </div>
+    ) : (
+        <div className='empty'>
+                <h2>No movies found!</h2>
+        </div>
+    )
+
+  }
+
+
+
+    
+    </div>
+
+
+)
+
+
+}
+
+
+export default App;
